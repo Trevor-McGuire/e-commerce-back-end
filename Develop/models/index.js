6e0a065a52639ba230b0ce34ead990687fd3f858
Trevor-McGuire <trevor.mcguire.candidate@gmail.com>
2023-07-27 00:29:15 -0500

    completed

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,14 +3,29 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const sequelize = require('../config/connection');
 
 // Products belongsTo Category
+Product.belongsTo(Category)
 
 // Categories have many Products
+Category.hasMany(Product)
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  }
+})
+// Product.hasMany(Tag)
+// Tag.hasMany(Product)
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag
+  }
+})
 
 module.exports = {
   Product,
